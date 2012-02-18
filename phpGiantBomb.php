b4041077ee49115817cc882e9327206265ddfc28
gpbmike <gpbmike@gmail.com>
2012-02-18 12:33:37 -0800

    option to use curl

--- phpGiantBomb.php
+++ phpGiantBomb.php
@@ -10,12 +10,61 @@ class phpGiantBomb {
     var $cached = array();
     var $cache = false;
     var $cache_dir;
-    
-    function phpGiantBomb ($api_key)
+    var $use_curl = false;
+
+    function phpGiantBomb ($api_key, $options)
     {
         $this->api_key = $api_key;
+        if ($options['use_curl']) {
+            $this->use_curl = true;
+        }
     }
-    
+
+
+    function curlURL($Url)
+    {
+        // is curl installed?
+        if (!function_exists('curl_init')){
+            die('CURL is not installed!');
+        }
+
+        // create a new curl resource
+        $ch = curl_init();
+
+        /*
+        Here you find more options for curl:
+        http://www.php.net/curl_setopt
+        */
+
+        // set URL to download
+        curl_setopt($ch, CURLOPT_URL, $Url);
+
+        // set referer:
+        // curl_setopt($ch, CURLOPT_REFERER, "http://www.google.com/");
+
+        // user agent:
+        // curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
+
+        // remove header? 0 = yes, 1 = no
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        // should curl return or print the data? true = return, false = print
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // timeout in seconds
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+        // download the given URL, and return output
+        $output = curl_exec($ch);
+
+        // close the curl resource, and free system resources
+        curl_close($ch);
+
+        // print output
+        return $output;
+    }
+
+
     function query ($url, $fields = array(), $limit = 10)
     {
         $fields = array_merge(array('api_key' => $this->api_key, 'format' => 'json', 'limit' => $limit), $fields);
@@ -25,7 +74,7 @@ class phpGiantBomb {
         /* not using caching, or there isn't a cache */
         if ($this->cache == false || !$cache) {
             $this->queries[] = $url;
-            $data = file_get_contents($url);
+            $data = $this->useCurl ? file_get_contents($url) : $this->curlURL($url);
             if ($this->cache && $data) {
                 $this->setCache($url, $data);
             }
@@ -37,7 +86,7 @@ class phpGiantBomb {
             return $cache;
         }
     }
-    
+
     function enableCache ($cache_dir = false, $cache_expire = 600)
     {
         $this->cache = true;
@@ -48,9 +97,9 @@ class phpGiantBomb {
                     unlink($this->cache_dir . '/' . $file);
                 }
             }
-        }    
+        }
     }
-    
+
     function getCache ($request)
     {
         if (!$this->cache) { return false; }
@@ -61,7 +110,7 @@ class phpGiantBomb {
         }
         return false;
     }
-    
+
     function setCache ($request, $response)
     {
         if (!$this->cache) { return false; }
@@ -72,250 +121,250 @@ class phpGiantBomb {
         fclose($fstream);
         return $result;
     }
-    
+
     function accessory ($id)
     {
         $fields = array();
         return $this->query('accessory/' . (int)$id . '/', $fields);
     }
-    
+
     function accessories ()
     {
         $fields = array();
         return $this->query('accessories/', $fields);
     }
-    
+
     function character ($id)
     {
         $fields = array();
         return $this->query('character/' . (int)$id . '/', $fields);
     }
-    
+
     function characters ()
     {
         $fields = array();
         return $this->query('characters/', $fields);
     }
-    
+
     function company ($id)
     {
         $fields = array();
         return $this->query('company/' . (int)$id . '/', $fields);
     }
-    
+
     function companies ()
     {
         $fields = array();
         return $this->query('companies/', $fields);
     }
-    
+
     function concept ($id)
     {
         $fields = array();
         return $this->query('concept/' . (int)$id . '/', $fields);
     }
-    
+
     function concepts ()
     {
         $fields = array();
         return $this->query('concepts/', $fields);
     }
-    
+
     function franchise ($id)
     {
         $fields = array();
         return $this->query('franchise/' . (int)$id . '/', $fields);
     }
-    
+
     function franchises ()
     {
         $fields = array();
         return $this->query('franchises/', $fields);
     }
-    
+
     function game ($id)
     {
         $fields = array();
         return $this->query('game/' . (int)$id . '/', $fields);
     }
-    
+
     function games ()
     {
         $fields = array();
         return $this->query('games/', $fields);
     }
-    
+
     function game_rating ($id)
     {
         $fields = array();
         return $this->query('game_rating/' . (int)$id . '/', $fields);
     }
-    
+
     function game_ratings ()
     {
         $fields = array();
         return $this->query('game_ratings/', $fields);
     }
-    
+
     function genre ($id)
     {
         $fields = array();
         return $this->query('genre/' . (int)$id . '/', $fields);
     }
-    
+
     function genres ()
     {
         $fields = array();
         return $this->query('genres/', $fields);
     }
-    
+
     function location ($id)
     {
         $fields = array();
         return $this->query('location/' . (int)$id . '/', $fields);
     }
-    
+
     function locations ()
     {
         $fields = array();
         return $this->query('locations/', $fields);
     }
-    
+
 /*
     function object ($id)
     {
         $fields = array();
         return $this->query('object/' . (int)$id . '/', $fields);
     }
-    
+
     function objects ()
     {
         $fields = array();
         return $this->query('objects/', $fields);
     }
 */
-    
+
     function person ($id)
     {
         $fields = array();
         return $this->query('person/' . (int)$id . '/', $fields);
     }
-    
+
     function people ()
     {
         $fields = array();
         return $this->query('people/', $fields);
     }
-    
+
     function platform ($id)
     {
         $fields = array();
         return $this->query('platform/' . (int)$id . '/', $fields);
     }
-    
+
     function platforms ()
     {
         $fields = array();
         return $this->query('platforms/', $fields);
     }
-    
+
     function rating_board ($id)
     {
         $fields = array();
         return $this->query('rating_board/' . (int)$id . '/', $fields);
     }
-    
+
     function rating_boards ()
     {
         $fields = array();
         return $this->query('rating_boards/', $fields);
     }
-    
+
     function region ($id)
     {
         $fields = array();
         return $this->query('region/' . (int)$id . '/', $fields);
     }
-    
+
     function regions ()
     {
         $fields = array();
         return $this->query('regions/', $fields);
     }
-    
+
     function release ($id)
     {
         $fields = array();
         return $this->query('release/' . (int)$id . '/', $fields);
     }
-    
+
     function releases ()
     {
         $fields = array();
         return $this->query('releases/', $fields);
     }
-    
+
     function review ($id)
     {
         $fields = array();
         return $this->query('review/' . (int)$id . '/', $fields);
     }
-    
+
     function reviews ()
     {
         $fields = array();
         return $this->query('reviews/', $fields);
     }
-    
+
     function search ($query)
     {
         $fields = array(
             'query' => $query );
         return $this->query('search/', $fields);
     }
-    
+
     function theme ($id)
     {
         $fields = array();
         return $this->query('theme/' . (int)$id . '/', $fields);
     }
-    
+
     function themes ()
     {
         $fields = array();
         return $this->query('themes/', $fields);
     }
-    
+
     function type ($id)
     {
         $fields = array();
         return $this->query('type/' . (int)$id . '/', $fields);
     }
-    
+
     function types ()
     {
         $fields = array();
         return $this->query('types/', $fields);
     }
-    
+
     function user_review ($id)
     {
         $fields = array();
         return $this->query('user_review/' . (int)$id . '/', $fields);
     }
-    
+
     function user_reviews ()
     {
         $fields = array();
         return $this->query('user_reviews/', $fields);
     }
-    
+
     function video ($id)
     {
         $fields = array();
         return $this->query('video/' . (int)$id . '/', $fields);
     }
-    
+
     function videos ()
     {
         $fields = array();
